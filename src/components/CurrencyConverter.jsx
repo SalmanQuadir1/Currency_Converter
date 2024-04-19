1cf6b5663eb5417e7d8fd3b5cd94ed92505c36c8
salman <najarsalman4@gmail.com>
2024-04-19 10:08:30 +0530

    new changes

--- src/components/CurrencyConverter.jsx
+++ src/components/CurrencyConverter.jsx
@@ -83,8 +83,8 @@ const CurrencyConverter = () => {
 
 
     return (
-        <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-xl shadow-lg ring-2 ring-cyan-500 ring-offset-2">
-            <h2 className='mb-5 text-2xl font-semibold text-gray-700'>Currency Converter</h2>
+        <div className="max-w-xl mx-auto my-10 p-5 bg-white rounded-xl shadow-lg  ring-4 ring-blue-700 ring-offset-2">
+            <h2 className='mb-5 text-2xl font-semibold text-gray-700 text-center'>Currency Converter</h2>
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-4 items-end">
                 <CurrencyDropdown
                     title={'From Currency'}
